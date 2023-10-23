e4fe9bf6cadbb7d32917ba2fabe5b150c2ef94e3
JustusOmbok <ombokjustus@gmail.com>
2023-10-23 14:49:51 +0300

    APIs in python

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" API to fetch empolee data and display it."""
 import requests
 from sys import argv
 
@@ -6,20 +7,16 @@ from sys import argv
 if __name__ == "__main__":
     _id = argv[1]
 
-
     base_url = "https://jsonplaceholder.typicode.com/"
     user_url = "{}users/{}".format(base_url, _id)
     tasks_url = "{}todos?userId={}".format(base_url, _id)
-
 
     response = requests.get(user_url)
     user_info = response.json()
     name = user_info["name"]
 
-
     response = requests.get(tasks_url)
     tasks = response.json()
-
 
     done_tasks = 0
     total_tasks = 0
@@ -30,7 +27,7 @@ if __name__ == "__main__":
             completed_titles.append(task["title"])
         total_tasks += 1
 
-
-    print("Employee {} is done with tasks({}/{}):".format(name, done_tasks, total_tasks))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, done_tasks, total_tasks))
     for title in completed_titles:
         print("\t{}".format(title))
