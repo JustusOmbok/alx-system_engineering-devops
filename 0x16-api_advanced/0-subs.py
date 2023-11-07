1aa14ca753b21558c3a524b2c645f3535d95e301
JustusOmbok <ombokjustus@gmail.com>
2023-11-07 13:19:09 +0300

    subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-import requests
+"""Module for subscribers number."""
 
 
 def number_of_subscribers(subreddit):
     """ Retrieves total suscribers on reddit."""
+    import requests
+
+    headers = {'User-Agent': 'Ombok)'}
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'MyBot/1.0 (by ombok)'}
+
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    if response.status_code >= 300:
         return 0
+
+    return response.json().get("data").get("subscribers")
