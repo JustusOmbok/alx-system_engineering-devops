9a95c38166cf623cc642805b0de35103aa6004a1
JustusOmbok <ombokjustus@gmail.com>
2023-11-07 18:27:38 +0300

    typo i Apache

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+exec { 'fixes_500_error':
+  command => "/bin/sed -i 's/\.phpp/\.php/g' /var/www/html/wp-settings.php",
+  path    => ["/bin"],
+  onlyif  => "/bin/test -f /var/www/html/wp-settings.php",
+}
