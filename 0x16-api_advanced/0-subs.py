ebeb7deda849333dba64f515b7e93ae7a1072acc
JustusOmbok <ombokjustus@gmail.com>
2023-11-07 13:07:07 +0300

    subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,9 +6,6 @@ def number_of_subscribers(subreddit):
     """ Retrieves total suscribers on reddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'MyBot/1.0 (by ombok)'}
-
-
-
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
