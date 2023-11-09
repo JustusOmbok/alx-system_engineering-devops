9de2222bb308f3aca12ef98c7d93f3cfead739ad
JustusOmbok <ombokjustus@gmail.com>
2023-11-09 12:17:12 +0300

    puppet

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 exec { 'fixes_500_error':
-  command => "/bin/sed -i 's/\.phpp/\.php/g' /var/www/html/wp-settings.php",
+  command => "/bin/sed -i 's/\\.phpp/\\.php/g' /var/www/html/wp-settings.php",
   path    => ["/bin"],
   onlyif  => "/bin/test -f /var/www/html/wp-settings.php",
 }
